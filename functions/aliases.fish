1408433da1bdd03809016882db3a60533e08393b
Aleksandr <4lex1v@gmail.com>
2016-07-11 23:26:00 -0400

    Some docker aliases + fisherman plugin manager

--- functions/aliases.fish
+++ functions/aliases.fish
@@ -12,6 +12,10 @@ alias bi 'brew info'
 alias bu 'brew update; and brew upgrade'
 alias bs 'brew search'
 
+alias dps 'docker ps'
+alias dml 'docker-machine ls'
+alias dmr 'docker-machien restart'
+
 alias sand 'cd ~/Sandbox'
 
 
